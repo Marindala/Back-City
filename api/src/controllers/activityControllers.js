55d9b0d174b95f91b5814e68c6d2e4f0d83ab0d7
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-27 23:46:32 -0300

    getAllActivities

--- api/src/controllers/activityControllers.js
+++ api/src/controllers/activityControllers.js
@@ -1,1 +1,17 @@
 //!better coding hay que lindo resalta  better comments
+const { Country, Activity } = require("../db");
+const { Op } = require("sequelize");
+
+const getAllActivities = async () => {
+  return await Activity.findAll({ 
+    include: {
+      model: Country, 
+      attributes: ["name"], 
+      through: { attributes: [] },
+    },
+  });
+};
+
+module.exports = {
+  getAllActivities,
+};
