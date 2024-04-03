2eeb8901436c0dc52fcae8b87a50b70097899491
Marina <95050756+Marindala@users.noreply.github.com>
2024-04-03 00:55:02 -0300

    index

--- api/index.js
+++ api/index.js
@@ -2,3 +2,14 @@
 //! resalta en rojo
 //? hola resalta en azul
 //* hola comenta en verde
+const server = require('./src/app..js');
+const { conn } = require('./src/db.js');
+require("dotenv").config();
+const {PORT} = process.env;
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log('%s listening at', PORT); // eslint-disable-line no-console
+  });
+});
